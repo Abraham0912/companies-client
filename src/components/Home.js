580e6b4a2349c4edadac35c0458cd8261fd11293
Abraham0912 <09120912ahn@gmail.com>
2021-11-02 14:27:16 -0600

    create

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Home component</h3>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
