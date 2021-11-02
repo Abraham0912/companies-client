106c74b9780102b0d481601635041a75d6af1f29
Abraham0912 <09120912ahn@gmail.com>
2021-11-02 14:27:37 -0600

    new

---
+++ src/layauts/LayautMain.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const LayautMain = ({children}) => {
+    return (
+        <div>
+            <h1>Welcome to LayautMain.js</h1>
+            {children}
+        </div>
+    )
+}
