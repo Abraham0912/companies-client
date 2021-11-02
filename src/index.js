ab219ffc56733ba8df53d0cc0f050a34c4deca6a
Abraham0912 <09120912ahn@gmail.com>
2021-11-02 15:00:03 -0600

    add styles in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Routes';
+import './assets/styles/general.sass'
+import './assets/styles/normalize.css'
 
 ReactDOM.render(
   <React.StrictMode>
