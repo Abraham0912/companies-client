7b32a9615fee8b13cd058bf941a906bbd3466fe9
Abraham0912 <09120912ahn@gmail.com>
2021-11-03 12:56:37 -0600

    clean code

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -9,14 +9,11 @@ export default class Edit extends Component {
         this.state={
             policyView:false,
             teleView: false,
-            developView: false,
-            // counter: 0
+            developView: false
         }
-        // this.incrementar=this.incrementar.bind(this)
     }
 
     toggleView(currentKey){
-        console.log("hola soy toggleView :D")
         Object.keys(this.state).forEach(key => {
             this.setState({
                 [key]: false
@@ -26,22 +23,13 @@ export default class Edit extends Component {
             [currentKey]: !this.state[currentKey]
         })
     }
-
-    // incrementar(){
-    //     this.setState({
-    //         counter:this.state.counter + 1
-    //     })
-    // }
   render() {
   console.log(this.state)
     return (
       <>
-      {/* <button onClick={this.incrementar}>Incrementar</button>
-      <h2>Contador= {this.state.counter}</h2> */}
         <div className="content">
             <div className="feature-section">
                 <FeatureList handleClick={this.toggleView.bind(this)}/>
-                <h1>pages/Edit.js</h1>
             </div>
         </div>
       </>
